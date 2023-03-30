5ee6cf9c650afb0208deee079840ee13000caeab
Сидни <just_siddy@mail.ru>
2023-03-30 22:11:52 +0300

    rewiew2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_ROOT = os.path.join(BASE_DIR, 'data/')
 
 
 SECRET_KEY = os.getenv(
--- backend/recipes/management/commands/loading_ingredients.py
+++ backend/recipes/management/commands/loading_ingredients.py
@@ -1,35 +1,51 @@
-import json
+import csv
+import sys
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
+    help = ('Import CSV-files. Use command: python3 manage.py importcsv')
+    BASES = {
+        'ingredients': Ingredient,
+        'tags': Tag,
+    }
+    FILES = {
+        'ingredients': f'{settings.DATA_ROOT}/ingredients.csv',
+        'tags': f'{settings.DATA_ROOT}/tags.csv',
+    }
 
-    def add_arguments(self, parser):
-        parser.add_argument("--path", type=str, help="file path")
+    def _get_or_create_object(self, model, data):
+        if model == Tag:
+            return model.objects.get_or_create(
+                                name=data['name'].lower(),
+                                slug=data['slug']
+                            )
+        return model.objects.get_or_create(
+                            name=data['name'].lower(),
+                            measurement_unit=data['measurement_unit']
+                        )
 
     def handle(self, *args, **options):
-        file_path = options["path"]
-
-        with open(file_path, encoding='utf-8') as f:
-            jsondata = json.load(f)
-            if 'color' in jsondata[0]:
-                for line in jsondata:
-                    if not Tag.objects.filter(
-                       slug=line['slug']).exists():
-                        Tag.objects.create(
-                            name=line['name'],
-                            color=line['color'],
-                            slug=line['slug'],
-                        )
-            elif 'measurement_unit' in jsondata[0]:
-                for line in jsondata:
-                    if not Ingredient.objects.filter(
-                       name=line['name'],
-                       measurement_unit=line['measurement_unit']).exists():
-                        Ingredient.objects.create(
-                            name=line['name'],
-                            measurement_unit=line['measurement_unit']
-                        )
+        for base_name, base in self.BASES.items():
+            with open(self.FILES[base_name], 'r', encoding="utf-8") as csvfile:
+                try:
+                    reader = csv.DictReader(csvfile, delimiter=';')
+                except Exception as e:
+                    sys.exit(f'CSV-file read exception {e}')
+                for row in reader:
+                    try:
+                        new_obj, status = self._get_or_create_object(base, row)
+                        if status:
+                            print(
+                                f'Создан новый объект: "{new_obj}"'
+                            )
+                        else:
+                            print(
+                                f'Объект "{new_obj}" создан ранее'
+                            )
+                    except Exception as e:
+                        print(f'Exception {e}')
