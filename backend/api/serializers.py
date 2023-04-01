f8e8adc914c1ebb8dcd9e11ca800a2ac17b6773f
Сидни <just_siddy@mail.ru>
2023-04-01 23:43:37 +0300

    review2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -20,10 +20,14 @@ class CustomUserSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request')
-        return (
-            request.is_authenticated
-            and request.follower.filter(user=request, author=obj.id).exists()
-        )
+        return request.is_authenticated and Subscription.objects.filter( 
+            user=request, author=obj.id).exists()
+        
+        #это вариант с релейтед неймом, который никак не получилось заставить работать
+        #return (
+        #    request.is_authenticated
+        #    and request.follower.filter(user=request, author=obj.id).exists()
+        #)
     
     def create(self, validated_data):
         user = User(
@@ -240,10 +244,14 @@ class ShowSubscriptionsSerializer(serializers.ModelSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request')
-        return (
-            request.is_authenticated
-            and request.follower.filter(user=request, author=obj.id).exists()
-        )
+        return request.is_authenticated and Subscription.objects.filter( 
+            user=request, author=obj.id).exists()
+    
+        #return (
+            #    request.is_authenticated
+            #    and request.follower.filter(user=request, author=obj.id).exists()
+        #)
+
 
     def get_recipes(self, obj):
         request = self.context.get('request')
