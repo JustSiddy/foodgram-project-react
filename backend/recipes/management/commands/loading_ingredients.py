518a4b472b845e7a4e92a8c038f950957840bda2
Сидни <just_siddy@mail.ru>
2023-04-01 19:12:46 +0300

    review2

--- backend/recipes/management/commands/loading_ingredients.py
+++ backend/recipes/management/commands/loading_ingredients.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
                 name = row
                 measurement_unit = row
                 Ingredient.objects.get_or_create(
-                    name=name, measurement_unit=measurement_unit,
+                    name=name[0], measurement_unit=measurement_unit[1],
                 )
         self.stdout.write(self.style.SUCCESS('Ингредиенты загружены.'))
 
@@ -28,6 +28,6 @@ class Command(BaseCommand):
                 color = row
                 slug = row
                 Tags.objects.get_or_create(
-                    name=name, color=color, slug=slug,
+                    name=name[0], color=color[1], slug=slug[2],
                 )
         self.stdout.write(self.style.SUCCESS('Теги загружены.'))
