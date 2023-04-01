c1e16777b94bcb816bf7583ad284a6fb1f4c7b83
Сидни <just_siddy@mail.ru>
2023-04-01 21:38:14 +0300

    rev

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = os.getenv(
 
 DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = ['158.160.3.114', 'backend', '127.0.0.1']
-
+ALLOWED_HOSTS = [os.getenv('HOSTS', default='backend')] 
+ 
 CSRF_TRUSTED_ORIGINS = ['http://158.160.3.114']
 
 INSTALLED_APPS = [
