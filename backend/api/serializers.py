9bd4d511dba882f0b21e634df3c8bb8f72e9dac4
Сидни <just_siddy@mail.ru>
2023-04-02 09:08:38 +0300

    review2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -28,8 +28,9 @@ class CustomUserSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request').user
-        return request.user.is_authenticated and Subscription.objects.filter( 
-            user=request.user, author=obj.id).exists()
+        if request is None or request.user.is_authenticated:
+            return request.user.is_authenticated and Subscription.objects.filter( 
+                user=request.user, author=obj.id).exists()
         
         #   это вариант с релейтед неймом, который никак не получилось заставить работать
         #   return (
@@ -240,8 +241,9 @@ class ShowSubscriptionsSerializer(serializers.ModelSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request').user
-        return request.user.is_authenticated and Subscription.objects.filter( 
-            user=request.user, author=obj.id).exists()
+        if request is None or request.user.is_authenticated:
+            return request.user.is_authenticated and Subscription.objects.filter( 
+                user=request.user, author=obj.id).exists()
         
         #   это вариант с релейтед неймом, который никак не получилось заставить работать
         #   return (
