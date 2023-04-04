32a0152cbf440dd23c227bef182bd881f9cc3d5c
Сидни <just_siddy@mail.ru>
2023-04-04 14:20:20 +0300

    r

--- backend/api/views.py
+++ backend/api/views.py
@@ -37,10 +37,9 @@ class SubscribeView(APIView):
         serializer = SubscriptionSerializer(
             data=data,
             context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def delete(self, request, id):
         author = get_object_or_404(User, id=id)
