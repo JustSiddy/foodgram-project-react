0d90b02c3ad5e7ae78a6aaf6847a6412b9b3f1c7
Сидни <just_siddy@mail.ru>
2023-04-05 23:55:44 +0300

    R

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -228,19 +228,20 @@ class ShowSubscriptionsSerializer(serializers.ModelSerializer):
             return request.follower.filter(
                 user=request.user, author=obj).exists()
 
-    def get_recipes(self, obj):
-        request = self.context.get('request')
-        if not request or request.user.is_anonymous:
-            return False
-        recipes = Recipe.objects.filter(author=obj)
-        limit = request.query_params.get('recipes_limit')
-        if limit:
-            recipes = recipes[:int(limit)]
-        return ShowFavoriteSerializer(
-            recipes, many=True, context={'request': request}).data
+    def get_recipes(self, obj): 
+        request = self.context.get('request') 
+        if not request or request.user.is_anonymous: 
+            return False 
+        recipes = obj.recipes.all() 
+        limit = request.query_params.get('recipes_limit') 
+        if limit: 
+            recipes = recipes[:int(limit)] 
+        return ShowFavoriteSerializer( 
+            recipes, many=True, context={'request': request}).data 
+ 
+    def get_recipes_count(self, obj): 
+        return obj.recipes.count() 
 
-    def get_recipes_count(self, obj):
-        return Recipe.objects.filter(author=obj).count()
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
