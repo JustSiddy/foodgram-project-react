69422d348c3f3f825082cc1d2c419be1211a7c61
Сидни <just_siddy@mail.ru>
2023-04-06 21:12:48 +0300

    R

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path 
 from rest_framework.routers import DefaultRouter 
  
-from api.views import (IngredientViewSet, RecipeViewSet, TagViewSet) 
+from api.views import (IngredientViewSet, RecipeViewSet, 
+                       ShowSubscriptionsView, SubscribeView, 
+                       TagViewSet)  
  
 app_name = 'api' 
  
@@ -11,8 +13,20 @@ router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register('recipes', RecipeViewSet, basename='recipes') 
 router.register('tags', TagViewSet, basename='tags') 
  
-urlpatterns = [ 
+userpatterns = [
+    path( 
+        'users/<int:id>/subscribe/', 
+        SubscribeView.as_view(), 
+        name='subscribe'), 
+    path( 
+        'users/subscriptions/', 
+        ShowSubscriptionsView.as_view(), 
+        name='subscriptions'), 
+]
+
+urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')), 
     path('', include('djoser.urls')), 
-    path('', include(router.urls)), 
-] 
+    path('', include(router.urls)),
+    path('users/', include(userpatterns))
+]
