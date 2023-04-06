e8deadd70471f44afe3a4ac017c345c7dd6f7a2a
Сидни <just_siddy@mail.ru>
2023-04-06 21:31:19 +0300

    R

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path 
 from rest_framework.routers import DefaultRouter 
  
-from api.views import (IngredientViewSet, RecipeViewSet, 
-                       ShowSubscriptionsView, SubscribeView, 
-                       TagViewSet)  
+from api.views import (FavoriteView, IngredientViewSet, RecipeViewSet, 
+                       ShoppingCartView, ShowSubscriptionsView, SubscribeView, 
+                       TagsViewSet, download_shopping_cart) 
  
 app_name = 'api' 
  
@@ -11,9 +11,21 @@ router = DefaultRouter()
  
 router.register('ingredients', IngredientViewSet, basename='ingredients') 
 router.register('recipes', RecipeViewSet, basename='recipes') 
-router.register('tags', TagViewSet, basename='tags') 
+router.register('tags', TagsViewSet, basename='tags') 
  
-userpatterns = [
+urlpatterns = [ 
+    path( 
+        'recipes/download_shopping_cart/', 
+        download_shopping_cart, 
+        name='download_shopping_cart'), 
+    path( 
+        'recipes/<int:id>/shopping_cart/', 
+        ShoppingCartView.as_view(), 
+        name='shopping_cart'), 
+    path( 
+        'recipes/<int:id>/favorite/', 
+        FavoriteView.as_view(), 
+        name='favorite'), 
     path( 
         'users/<int:id>/subscribe/', 
         SubscribeView.as_view(), 
@@ -22,11 +34,7 @@ userpatterns = [
         'users/subscriptions/', 
         ShowSubscriptionsView.as_view(), 
         name='subscriptions'), 
-]
-
-urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')), 
     path('', include('djoser.urls')), 
-    path('', include(router.urls)),
-    path('users/', include(userpatterns))
+    path('', include(router.urls)), 
 ]
