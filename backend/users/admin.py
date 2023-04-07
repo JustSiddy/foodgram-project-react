ff73174015ec7b3602a6d43855da015151216d70
Сидни <just_siddy@mail.ru>
2023-04-07 23:31:05 +0300

    review2

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -14,7 +14,7 @@ class UserAdmin(UserAdmin):
     empty_value_display = '-пусто-' 
  
     def get_recipes_count(self, obj): 
-        return obj.recipe.count() 
+        return obj.user.count() 
     get_recipes_count.short_description = 'Рецепты' 
  
     def get_followers_count(self, obj): 
